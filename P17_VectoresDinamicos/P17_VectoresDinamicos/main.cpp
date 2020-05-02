a655877a07ee5cfaeb5bebf6e7caa80b757b3585
Erik <estizubi@ucm.es>
2020-05-02 21:53:46 +0200

    P17

--- P17_VectoresDinamicos/P17_VectoresDinamicos/main.cpp
+++ P17_VectoresDinamicos/P17_VectoresDinamicos/main.cpp
@@ -7,35 +7,112 @@
 #include <fstream>
 
 using namespace std;
-
+const int TAM_INICIAL = 3;
 typedef struct {
 	string nombre;
 	int unidades;
 }tObj;
+typedef struct {
+	tObj* arrayDinam;
+	// tam = número de elementos
+	// capacidad = capacidad total
+	int tam, capacidad;
+}tLista;
 
-void resolver() {
-
+bool encontrado(const tLista& lista, string& nombre, int& pos) {
+	// utilizamos selección con intercambios para buscar el elemento
+	// devolvemos true si lo ha encontrado y la posición por parámetro
+	bool encontrado = false;
+	int i = 0;
+	while (!encontrado && i < lista.tam) {
+		if (lista.arrayDinam[i].nombre == nombre)
+			encontrado = true;
+		else
+			++i;
+	}
+	pos = i;
+	return encontrado;
 }
 
-void visualizar(tObj* ptr, int tam) {
-	for (int i = 0; i < tam; ++i) {
-		cout << ptr[i].nombre << " " << ptr[i].unidades << endl;
+void visualizar(const tLista& lista) {
+	// por cada elemento que hay en el array dinámico, visualizamos el nombre y unidades
+	for (int i = 0; i < lista.tam; ++i) {
+		cout << lista.arrayDinam[i].nombre << " " << lista.arrayDinam[i].unidades << endl;
 	}
 	cout << "---\n";
 }
 
-bool resuelveCaso() {
-	int tam = 3, cont = 0;
-	tObj* ptr = new tObj[tam];
-	char n;
-	cin.get(n);
-	if (n == 'F')
-		return false;
-	// operaciones
-	if (n == 'L')
-		visualizar(ptr, tam);
-	delete[]ptr;
-	return true;
+void ampliar(tLista& lista) {
+	// creamos un puntero auxiliar y lo inicializamos a null
+	tObj* aux = nullptr;
+	// ese aux va a apuntar a un array con tamaño doble del array que teníamos
+	aux = new tObj[2 * lista.capacidad];
+	// ahora copia todos los elementos que había en el array antiguo
+	for (int i = 0; i < lista.tam; ++i) {
+		aux[i] = lista.arrayDinam[i];
+	}
+	// liberamos memoria
+	delete[]lista.arrayDinam;
+	// volvemos a pasar la información de aux a lista
+	lista.arrayDinam = aux;
+	// aumentamos la capacidad
+	lista.capacidad *= 2;
+	// aux vuelve a apuntar a null
+	aux = nullptr;
+}
+
+void resuelveCaso() {
+	tLista lista;
+	// inicializamos
+	lista.capacidad = TAM_INICIAL;
+	lista.tam = 0;
+	lista.arrayDinam = new tObj[TAM_INICIAL];
+	char a, aux;
+	string nombre;
+	int pos = 0;
+	cin >> a;
+	// si leemos F, se finaliza el programa
+	while (a != 'F') {
+		// visualizamos el array que tenemos
+		if (a == 'L')
+			visualizar(lista);
+		else {
+			int num = 0;
+			cin >> nombre >> num;
+			if (encontrado(lista, nombre, pos)) {
+				// aumentamos las unidades de un producto existente
+				if (a == 'A')
+					lista.arrayDinam[pos].unidades += num;
+				// restamos las unidades de un producto existente
+				else if (a == 'R') {
+					// si las unidades que le vamos a restar son mayores de las que hay, ponemos a 0
+					if (lista.arrayDinam[pos].unidades < num)
+						lista.arrayDinam[pos].unidades = 0;
+					else
+						// restamos la cantidad en caso contrario
+						lista.arrayDinam[pos].unidades -= num;
+				}
+			}
+			// si el objeto no existe y le estamos sumando lo añadimos al array
+			else if (!encontrado(lista, nombre, pos) && a == 'A'){
+				// si el array se ha llenado, hay que aumentar su capacidad
+				if (lista.tam == lista.capacidad - 1)
+					ampliar(lista);
+				// le asignamos un nombre y unas unidades al nuevo objeto
+				lista.arrayDinam[lista.tam].nombre = nombre;
+				lista.arrayDinam[lista.tam].unidades = num;
+				// el número de elementos de la lista aumenta por uno
+				++lista.tam;
+			}
+		}
+		// salto de línea
+		cin.get(aux);
+		cin >> a;
+	}
+	// liberamos la memoria
+	delete[] lista.arrayDinam;
+	// el puntero ahora apunta a una dirección nula
+	lista.arrayDinam = nullptr;
 }
 
 int main() {
@@ -47,7 +124,7 @@ int main() {
 	auto coutbuf = std::cout.rdbuf(out.rdbuf());
 #endif
 
-	while (resuelveCaso());
+	resuelveCaso();
 
 	// para dejar todo como estaba al principio
 #ifndef DOMJUDGE
